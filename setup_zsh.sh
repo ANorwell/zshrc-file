b18e0bdca8db3f800bb6d976a36557c5c16d3892
Arron Norwell <anorwell@gmail.com>
2020-06-24 16:09:11 -0400

    Create setup_zsh.sh
    Set up a nice shell environment in a single script

---
+++ setup_zsh.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env zsh
+
+# Install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install some plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zdharma/fast-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
+sed -i -e 's/^plugins=.*/plugins=(git z fast-syntax-highlighting zsh-autosuggestions)/g' ~/.zshrc
+sed -i -e 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+
+
+cat > $HOME/.custom.zsh <<- EOM
+alias gd='git diff'
+alias gs='git status'
+alias gco='git checkout'
+alias gcm='git commit'
+alias gl='git lg'
+alias gsh='git show'
+alias gad='git add -A'
+alias gr='git recent'
+alias gp='git push'
+alias gf='git fetch'
+alias gfp='git pull'
+alias gfm='gco master; gf'
+alias grb='git rebase'
+alias gpoh='gp -u origin HEAD'
+alias gplh='gp -u lego HEAD'
+alias gpah='gp -u anorwell HEAD'
+
+export PATH=$PATH:"$HOME/.local/bin"
+
+EOM
+
+echo '[ -f ~/.custom.zsh ] && source ~/.custom.zsh' >> ~/.zshrc
+
+cat > $HOME/.gitconfig <<- EOM
+[alias]
+        lg1 = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)'
+        lg2 = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'
+        lg = !"git lg1"
+        recent = "for-each-ref --sort=-committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'" --count 12
+[user]
+        name = Arron Norwell
+        email = anorwell@datto.com
+[core]
+        editor = mg
+        excludesfile = /Users/anorwell/.gitignore_global
+EOM
+
+source ~/.zshrc
+
+# install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+p10k configure
